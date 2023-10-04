2569c30aac1010c376b3e74864439c435e56ab84
David <mttai@cmcglobal.vn>
2023-10-04 14:53:01 +0700

    init commit

---
+++ src/main/java/com/apim/apimfacadepattern/ApimFacadePatternApplication.java
@@ -0,0 +1,13 @@
+package com.apim.apimfacadepattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimFacadePatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApimFacadePatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apim/apimfacadepattern/ApimFacadePatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apim.apimfacadepattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimFacadePatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
